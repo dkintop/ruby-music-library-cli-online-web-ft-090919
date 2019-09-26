bb8f7eec32764a665c1f528d3fdd2fb3d9afd41a
Doug Kintop <dkintop@hotmail.com>
2019-09-26 22:34:44 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -81,15 +81,7 @@ class MusicLibraryController
   end 
   
   
-  def play_song
-    puts "Which song number would you like to play?"
-    input =gets.chomp.to_i
-    #binding.pry
-    song = list_songs[input - 1]
-    puts "Playing #{song.name} by #{song.artist.name}"
-     
-    
-  end 
+  
   
   
 end
