cb5f49e3407ad1eb624501dcace24280e3ad2b70
Doug Kintop <dkintop@hotmail.com>
2019-09-26 21:54:23 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -54,7 +54,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     artist = gets.chomp
     
-    song_list = Song.all.select{|song| song.artist.name == artist binding.pry}
+    song_list = Song.all.select {|song| song.artist.name == artist binding.pry}
     song_list.
   end 
   
