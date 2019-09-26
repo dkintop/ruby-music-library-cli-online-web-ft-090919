54ae81c322cc5e7abeb25af92e175a7614c1b17a
Doug Kintop <dkintop@hotmail.com>
2019-09-26 19:29:15 +0000

    Automatically backed up by Learn

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -1,8 +1,8 @@
 module Concerns::Findable
   
  def find_by_name(name)
-    all.find do |object|
-        object.name == name
+    all.find do |instance|
+        instance.name == name
     end
   end
   
