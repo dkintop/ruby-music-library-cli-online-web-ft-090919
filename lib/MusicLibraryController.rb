02b211223af7557d3b1f426fb63e2fb97b25735f
Doug Kintop <dkintop@hotmail.com>
2019-09-26 22:24:22 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -84,8 +84,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input =gets.chomp
-    list_songs 
-    binding.pry
+     
+    
   end 
   
   
