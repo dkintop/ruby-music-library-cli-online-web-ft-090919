60b7fff1d2678e1eed31c5f84b429be11ce95793
Doug Kintop <dkintop@hotmail.com>
2019-09-26 20:27:05 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -16,8 +16,10 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.chomp
-    if input != 'exit'
+    unless input == 'exit' do 
       input = gets.chomp
+    
+    
     end
   end
   
