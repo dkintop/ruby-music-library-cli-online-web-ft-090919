1f0990dbe6b2698f9793547165eaa452920a0a08
Doug Kintop <dkintop@hotmail.com>
2019-09-26 21:43:52 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -33,13 +33,13 @@ class Artist
     @songs
   end
 
- #this sets up the song belongs to the artist association
+ 
   def add_song(song)
     song.artist = self unless song.artist == self
     @songs << song unless @songs.include?(song)
   end
 
-  #artist has many genres through songs
+ 
   def genres
     genres = @songs.collect do |song|
       song.genre
