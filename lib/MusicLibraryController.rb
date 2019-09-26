af2c712afc93c84489abd3010b405a686d5edd6d
Doug Kintop <dkintop@hotmail.com>
2019-09-26 23:52:18 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -30,6 +30,8 @@ class MusicLibraryController
         self.list_artists
       when 'list genres'
         self.list_genres 
+      when 'list artist'
+        self.list_songs_by_artist
       #when
       
       
