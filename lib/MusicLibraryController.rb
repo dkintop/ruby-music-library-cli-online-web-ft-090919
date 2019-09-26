07cd688223df300b34712976993367287a16313d
Doug Kintop <dkintop@hotmail.com>
2019-09-26 21:57:24 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -50,12 +50,7 @@ class MusicLibraryController
     end
   end 
   
-  def list_songs_by_artist
-    puts "Please enter the name of an artist:"
-    artist = gets.chomp
-    
-    
-  end 
+  
   
   
 end
