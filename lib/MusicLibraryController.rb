95fa51585cebec2c200e8fac51f10c660f63691b
Doug Kintop <dkintop@hotmail.com>
2019-09-26 21:21:54 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -26,11 +26,11 @@ class MusicLibraryController
   end
   
   def list_songs
-   songs_sorted_by_name = Song.all.sort_by do |song|
+   sorted_songs = Song.all.sort_by do |song|
       song.name
     end
     #binding.pry
-    songs_sorted_by_name.uniq.each.with_index(1) do |song,index|
+    sorted_songs.uniq.each.with_index(1) do |song,index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
